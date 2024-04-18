c7842c839cc53c3eee191524d6edb1da1c829609
GustavoCaruzoGoncalves <gustavo.caruzo.goncalves@gmail.com>
2024-04-18 15:04:42 -0300

    added makefile to  target

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TokyoPro.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TokyoPro-user \
+    omni_TokyoPro-userdebug \
+    omni_TokyoPro-eng
